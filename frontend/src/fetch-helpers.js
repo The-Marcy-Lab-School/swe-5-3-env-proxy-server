c5913b8b04919b7595c3d7c1029ac9e53bc8ed54
benspector-mls <ben@marcylabschool.org>
2026-03-09 11:30:35 -0400

    minor update to url

--- frontend/src/fetch-helpers.js
+++ frontend/src/fetch-helpers.js
@@ -2,7 +2,7 @@ const API_KEY = 'How can I include this without exposing it to the public?';
 
 export const getTrendingGifs = async () => {
   try {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}&limit=3`);
+    const response = await fetch(`https://api.giphy.com/v1/gifs/trending?limit=3&rating=g&api_key=${API_KEY}`);
 
     if (!response.ok) {
       throw new Error(`Fetch failed with status - ${response.status}, ${response.statusText}`);
@@ -19,7 +19,7 @@ export const getTrendingGifs = async () => {
 
 export const getGifsBySearch = async (searchTerm) => {
   try {
-    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&limit=3&q=${searchTerm}`);
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?limit=3&rating=g&api_key=${API_KEY}&q=${searchTerm}`);
 
     if (!response.ok) {
       throw new Error(`Fetch failed with status - ${response.status}, ${response.statusText}`);
